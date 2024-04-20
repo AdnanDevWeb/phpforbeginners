293669cb5ecfe716103298d66f1e96f51b430a7c
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com~>
2024-04-20 20:59:48 +0200

    partials done

---
+++ views/index.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-100">
+
+<head>
+    <title>Demo</title>
+    <meta charset="utf-8">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="h-full">
+    <div class="min-h-full">
+        <?php require("partials/nav.php"); ?>
+
+        <?php require("partials/header.php"); ?>
+        <?php require("partials/main.php"); ?>
+    </div>
+
+</body>
+
+</html>
